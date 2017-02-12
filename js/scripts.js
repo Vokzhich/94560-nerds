295e5c809dee08f08985db824af8a22317e1a8f5
Vokzhich <vokzhich@gmail.com>
2017-02-12 15:47:33 +0300

    Версия верстки со скриптами
    Провел все через валидатор внес необходимые изменения

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+
+/*Variables*/
+var link = document.querySelector(".modal-on-button");
+var popup = document.querySelector(".modal-container");
+var closeButton = popup.querySelector(".close-button-modal");
+var modalName = popup.querySelector("[name=name]");
+var modalEmail = popup.querySelector("[name=email]");
+var modalText = popup.querySelector("[name=text]");
+var formModal = popup.querySelector("#modal-form");
+var formElements = formModal.querySelector("[type=text]");
+/*Variables*/
+
+/*Yandex maps*/
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
             center: [59.938631, 30.323000],
@@ -11,18 +24,45 @@ ymaps.ready(function () {
             hintContent: 'Nerds',
 
         }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
             iconLayout: 'default#image',
-            // Своё изображение иконки метки.
             iconImageHref: 'img/myicon.png',
-            // Размеры метки.
             iconImageSize: [155, 200],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
             iconImageOffset: [-50,-199]
         });
 
     myMap.geoObjects.add(myPlacemark);
     myMap.behaviors.disable('scrollZoom');
 });
+/*Yandex maps*/
+
+/*Modal window opener*/
+link.addEventListener("click",function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-container-on");
+    modalName.focus();
+});
+
+closeButton.addEventListener("click",function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-container-on");
+});
+/*Modal window opener*/
+
+
+/*Modal window form validation*/
+    formModal.addEventListener("submit", function(event) {
+       event.preventDefault();
+       if (this.querySelector("[name=name]").value == "") {
+            this.querySelector("[name=name]").classList.add("error");
+       }else if (this.querySelector("[name=name]").value) {
+            this.querySelector("[name=name]").classList.remove("error");
+       };
+       if (this.querySelector("[name=email]").value == "") {
+            this.querySelector("[name=email]").classList.add("error");
+       }else if (this.querySelector("[name=email]").value) {
+            this.querySelector("[name=email]").classList.remove("error");
+       };
+    });
+/*Modal window form validation*/
+
+
