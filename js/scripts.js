f94a6fb9f2b5cc9c535c47b2173415725564a4e9
Vokzhich <vokzhich@gmail.com>
2017-02-20 16:52:35 +0300

    Внес изменения после попытки номер 1

--- js/scripts.js
+++ js/scripts.js
@@ -12,58 +12,80 @@ var formElements = formModal.querySelector("[type=text]");
 
 /*Yandex maps*/
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [59.939000, 30.319222],
-            zoom: 17,
-            controls: [],
-            scroll: false
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        myPlacemark = new ymaps.Placemark([59.938631, 30.321239], {
-            hintContent: 'Nerds',
+		var myMap = new ymaps.Map('map', {
+						center: [59.939000, 30.319580],
+						zoom: 17,
+						controls: [],
+						scroll: false
+				}, {
+						searchControlProvider: 'yandex#search'
+				}),
+				myPlacemark = new ymaps.Placemark([59.938631, 30.321239], {
+						hintContent: 'Nerds',
 
-        }, {
-            iconLayout: 'default#image',
-            iconImageHref: 'img/myicon.png',
-            iconImageSize: [155, 200],
-            iconImageOffset: [-50,-199]
-        });
+				}, {
+						iconLayout: 'default#image',
+						iconImageHref: 'img/myicon.png',
+						iconImageSize: [155, 200],
+						iconImageOffset: [-50,-199]
+				});
 
-    myMap.geoObjects.add(myPlacemark);
-    myMap.behaviors.disable('scrollZoom');
+		myMap.geoObjects.add(myPlacemark);
+		myMap.behaviors.disable('scrollZoom');
 });
 /*Yandex maps*/
 
 
 /*Modal window opener*/
 link.addEventListener("click",function(event) {
-    event.preventDefault();
-    popup.classList.add("modal-container-on");
-    modalName.focus();
+		event.preventDefault();
+		popup.classList.add("modal-container-on");
+		modalName.focus();
 });
 
 closeButton.addEventListener("click",function(event) {
-    event.preventDefault();
-    popup.classList.remove("modal-container-on");
+		event.preventDefault();
+		popup.classList.remove("modal-container-on");
+		popup.classList.remove("modal-error");
 });
 /*Modal window opener*/
 
 
 /*Modal window form validation*/
-    formModal.addEventListener("submit", function(event) {
-       event.preventDefault();
-       if (this.querySelector("[name=name]").value == "") {
-            this.querySelector("[name=name]").classList.add("error");
-       }else if (this.querySelector("[name=name]").value) {
-            this.querySelector("[name=name]").classList.remove("error");
-       };
-       if (this.querySelector("[name=email]").value == "") {
-            this.querySelector("[name=email]").classList.add("error");
-       }else if (this.querySelector("[name=email]").value) {
-            this.querySelector("[name=email]").classList.remove("error");
-       };
-    });
+
+formModal.addEventListener("submit", function(event) {
+	if (!modalName.value || !modalEmail.value || !modalText.value) {
+		event.preventDefault();
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
+	};
+	if (this.querySelector("[name=name]").value == "") {
+			this.querySelector("[name=name]").classList.add("error");
+		 }else if (this.querySelector("[name=name]").value) {
+				this.querySelector("[name=name]").classList.remove("error")
+		 };
+	if (this.querySelector("[name=email]").value == "") {
+			this.querySelector("[name=email]").classList.add("error")
+		}else if (this.querySelector("[name=email]").value) {
+				this.querySelector("[name=email]").classList.remove("error")
+		};
+	if (this.querySelector("[name=text]").value == "") {
+			this.querySelector("[name=text]").classList.add("error")
+	}else if(this.querySelector("[name=text]").value) {
+			this.querySelector("[name=text]").classList.remove("error") 
+	};
+});
 /*Modal window form validation*/
 
+/*Modal window ESC close*/    
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("modal-container-on")) {
+			popup.classList.remove("modal-container-on");
+			popup.classList.remove("modal-error");
+		}
+	}
+});
+/*Modal window ESC close*/
 
